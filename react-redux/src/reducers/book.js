e9ac816b256faa579f524bb1e75efe3d0ac6fb13
TrungNV <trungnv@egdgroup.com>
2021-10-12 00:29:10 +0700

    refector

--- react-redux/src/reducers/book.js
+++ react-redux/src/reducers/book.js
@@ -4,7 +4,7 @@ const initBook =[
     {id: 3, name: 'You!!', status: 0},
 ];
 var nexBookId = initBook.length;
-const renderBookId = (books, bookId)  => {
+const getIndexOfBook = (books, bookId)  => {
     for(var i = 0; i < books.length; i++) {
         var book = books[i];
         if(book.id === bookId) {
@@ -22,14 +22,14 @@ export const bookReducer = (state = initBook, action) => {
             books.push({id: nexBookId, name: '',status: 0});
             break;
         case "EDIT":
-            var bookId = renderBookId(books,action.data.bookId);
+            var bookId = getIndexOfBook(books,action.data.bookId);
             if(bookId != -1)  {
                 books[bookId].name = action.data.name;
                 books[bookId].status = action.data.status;
             }
             break;
         case "DELETE":
-            var bookId = renderBookId(books,action.bookId);
+            var bookId = getIndexOfBook(books,action.bookId);
             if(bookId != -1){
                 books.splice(bookId,1)
             }
